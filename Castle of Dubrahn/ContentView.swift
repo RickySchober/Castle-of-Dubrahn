7c76ea61464560af393ebdd50bcb9010c5042b0d
Ricky Schober <rickylschober@gmail.com>
2020-06-28 14:08:29 -0700

    Initial Commit

---
+++ Castle of Dubrahn/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Castle of Dubrahn
+//
+//  Created by Ricky Schober on 6/28/20.
+//  Copyright © 2020 Ricky Schober. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
